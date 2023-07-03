149a928231ee961787a226f00c5610556f8e53da
Ashish <ajerath@innow8apps.com>
2023-07-03 10:04:11 +0530

    Update index.js

--- src/shared/layout/index.js
+++ src/shared/layout/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const Layout = (props) => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.loginReducer.user_details);
+
+  useEffect(() => {
+    if (user.id !== undefined) {
+      navigate("/");
+    }
+  }, [user]);
+
   return (
     <div className="main-layout">
       <NavBar />
